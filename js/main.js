0c864e57abddd3faa9847b7947ea2bee0fc9d2e6
Amanda Souza <nanda.hamandadesouza@gmail.com>
2021-08-27 23:16:29 -0400

    Fazendo o Fetch

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+'use strict';
 
-const pesquisarCep = () =>{
-    alert ("Ola Mundo")
+const pesquisarCep = async() =>{
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    console.log(endereco)
 }
 
 document.getElementById('cep')
